d619c768b28816f429cfd35bc73d1dd7302e0ad4
Artur-Pastana <reitutu_15@hotmail.com>
2020-10-29 21:06:14 -0300

    Primeira Classe: Posicao

--- aplication/Programa.java
+++ aplication/Programa.java
@@ -1,7 +1,12 @@
 package secao16.aplication;
 
+import secao16.boardgame.Posicao;
+
 public class Programa {
     public static void main(String[] args) {
         
+        Posicao posicao = new Posicao(10, 20);
+
+        System.out.println(posicao);
     }
 }
---
+++ boardgame/Posicao.java
@@ -0,0 +1,30 @@
+package secao16.boardgame;
+
+public class Posicao {
+    private int linha;
+    private int coluna;
+
+    public Posicao(int linha, int coluna){
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+    public int getLinha() {
+        return linha;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }
+    public void setLinha(int linha) {
+        this.linha = linha;
+    }
+
+    @Override
+    public String toString() {
+        return getLinha()+"," +getColuna();
+    }
+}
